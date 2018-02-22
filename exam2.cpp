cace9eb8210cf2f008debe0f892b9db507b6f7bb
Unknown <changyom12.teas@gmail.com>
2018-02-22 12:26:54 +0700

    ข้อสอบข้อที่ 2
    ทำเกี่ยวกับการจัดการรายชื่อนักเรียน โดยมีการเขียนเมนูให้ผู้ใช้เลือก คือ 1. เลือกรายชื่อ 2. รายการรายชื่อ
    3.การค้นหาชื่อนักเรียน 4. ออกจากฟังก์ชัน
    เมือเลือกแล้วจะต้องเข้าสู่หน้าเมนู มีการเขียนไฟล์และบันทึกไฟล์
    
    
    
    
    
    
    
    
    
    
    
    
    
    #อาจารย์ตามที่อาจารย์ได้บอกไว้ว่า อ.ได้ช่วยพวกหนูแล้ว โดยการให้ไฟล์ตัวอย่าง แล้วให้ไปหาคำตอบเอง และจะให้ใครช่วยก็ได้หนูได้ให้เพื่อนช่วยมา โดยมีการส่งคำตอบมาให้ล่วงหน้า หนูจึงใช้สิทธิ์ที่คิดว่าน่าจะไม่มีเปิดดูแล้ว
    
    
    
    
    
    ขอโทษค่ะ

--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include<string>
+using namespace std;
 
 int main() {
+    int cmd;
+    string name;
+    string line;
+    ofstream writeFile;
+    ifstream readFile;
+    do{
+        cout << "MENU" <<endl;
+        cout << "1.Add student." <<endl;
+        cout << "2.List student." <<endl;
+        cout << "3.Search List student." <<endl;
+        cout << "4.Quit." <<endl;
+        cout << "Please Enter MENU : ";
+        cin >> cmd;
+        if (cmd==1){
+            cout << "Enter std name : ";
+            cin >> name;
+            writeFile.open("std.txt.fstream::app");
+            if(writeFile.is_open()){
+                writeFile << name << endl;
+            }else{
+                cout << "unable to open file";
+            }
+        }else if (cmd==2){
+            readFile.open("std.txt");
+            if(readFile.is_open()){
+                while(getline(readFile,line)){
+                    cout << line << endl;}
+            }
+        }
+
+        else if (cmd==3){
+            cout << "Enter name : ";
+            cin >> name;
+            readFile.open("std.txt");
+            if(readFile.is_open()){
+                (name = "&name&",name);
+                while (getline(readFile,name)){
+                    cout << name << endl;
+                }
+            }
+        }else{
+        break;}
+    }while(myfile.close);
+
+
 
         return 0;
 }
